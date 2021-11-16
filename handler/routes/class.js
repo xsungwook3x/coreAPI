69d8d0a277a22356b085b4ec5fb72f480e564484
xsungwook3x <xsungwook3x@naver.com>
2021-11-16 00:49:11 +0000

    클래스 부분 추가

--- handler/routes/class.js
+++ handler/routes/class.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Math=require('Math');
 
 //const jwt = require('jsonwebtoken');
 const model = require('../model/model.js');
@@ -20,12 +19,12 @@ router.post('/addClass', function(req,res,next){//클래스만들기
         .then(result => {
             if(result === null) throw new Error('invalid-user');
             if(result.role !== 1) throw new Error('role-auth-fail');
-
+            const classId=Math.random().toString(36).substr(2,11);
             const save_classroom = model.classroom({
                 name : req.body.name,
                 classroom_master : user_id,
                 user_list : [],
-                class_id:Math.random().toString(36).substr(2,11),
+                class_id:classId,
                 request_student_list: [],
                 problem_list:[]
             });
@@ -90,5 +89,58 @@ router.get('/getClasses/student',function(req,res,next){//학생이 참여한 �
 });
 
 router.get('/getClasses/teacher',function(req,res,next){
-    
+    const user_nick=req.body.user_nick;
+
+    model.classroom.find()
+    .where('class_master').equals(user_nick)
+    .then(result => {
+        if(result === null ) throw new Error('invalid classes');
+        res.status(200).json(result)
+    }).catch(err => {
+        if(err.message === 'invalid class'){
+            res.status(400).json({message:'there isnt classes'})
+        }else{
+            res.status(500).json({message:'server-error'});
+        }
+    })
 })
+
+router.post('/addBelonged',function(req,res,next){//클래스 가입하면 추가되는거 구현
+    const user_nick=req.body.user_nick;
+    const class_id=req.body.class_id;
+
+    model.user.find()
+    .update(
+        { nick: user_nick }, 
+        { $push: { belonged_classes: class_id } })
+    .then(result => {
+        if(result === null ) throw new Error('추가실패');
+        res.status(200).json({message:'추가성공'})
+    }).catch(err =>{
+        if(err.message === 'invalid class'){
+            res.status(400).json({message:'추가실패'});
+        }else{
+            res.status(500).json({message:'server-error'});
+        }
+    })
+
+} );
+
+router.delete('/',function(req,res,next){//삭제
+    const class_id=req.body.class_id;
+
+    model.classroom.deleteOne()
+    .where('class_id').equals(class_id)
+    .then(result => {
+        if(result ===null) throw new Error('삭제 실패');
+        res.status(200).json({message:'삭제성공'});
+    }).catch(err => {
+        if(err.message === '삭제 실패'){
+            res.status(400).json({message:'삭제실패'});
+        }else{
+            res.status(500).json({message:'server-error'});
+        }
+    })
+})
+
+module.exports = router;
